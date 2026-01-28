65de6e4c0e05309d3dad31f9d8256a0f1b214ba3
taiyakihitotsu <taiyakihitotsu@proton.me>
2026-01-28 20:15:46 +0900

    wip

---
+++ test-d/internal/unique-union-deep.ts
@@ -0,0 +1,30 @@
+import {expectType} from 'tsd';
+import type {UniqueUnionDeep} from '../../source/internal/type.d.ts';
+
+// The returns of `UniqueUnionDeep` are expected to be equal of `UniqueUnion`, if there's no recursive object type in a passed union type.
+// That's why those tests are pasted from 'test-d/internal/union-unique.ts'.
+
+expectType<never>({} as UniqueUnionDeep<never>);
+expectType<string>({} as UniqueUnionDeep<string>);
+expectType<{a: 0}>({} as UniqueUnionDeep<{a: 0}>);
+expectType<unknown>({} as UniqueUnionDeep<unknown>);
+expectType<any>({} as UniqueUnionDeep<any>);
+expectType<[unknown]>({} as UniqueUnionDeep<[unknown]>);
+expectType<[any]>({} as UniqueUnionDeep<[any]>);
+
+expectType<{a: 0} | {a: 1}>({} as UniqueUnionDeep<{a: 0} | {a: 0} | {a: 0} | {a: 1}>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
+
+// `{a: t}` isn't excluded with `{a: T}` even if `T` includes `t`.
+expectType<{a: 0} | {a: 1} | {a: number}>({} as UniqueUnionDeep<{a: 0} | {a: 0} | {a: 0} | {a: 1} | {a: number}>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
+
+// `readonly`, `optional`, both, and general object key shouldn't be mutually equal.
+expectType<{a: number} | {readonly a: number} | {a?: number} | {readonly a?: number}>({} as UniqueUnionDeep<{a: number} | {readonly a: number} | {a?: number} | {readonly a?: number} | {a: number} | {readonly a: number} | {a?: number} | {readonly a?: number}>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
+
+// Empty tuple isn't removed by `T[]`.
+expectType<[0, 1, 2] | [0, 1] | [] | number[]>({} as UniqueUnionDeep<[0, 1, 2] | [0, 1, 2] | [0, 1, 2] | [0, 1] | [] | number[]>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
+
+// `T[]` doesn't delete tuples of `[t, ...t]` even if `T` includes `t`.
+expectType<[0, 1, 2] | ['0', unknown] | [0, unknown]>({} as UniqueUnionDeep<[0, 1, 2] | [0, 1, 2] | [0, 1, 2] | [0, unknown] | ['0', unknown]>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
+
+// `[u, ...u]` doesn't deletes tuple of `[v, ...y]` even if `u` inclues `v`.
+expectType<[0, 1, 2] | ['0', unknown] | [0, unknown] | number[]>({} as UniqueUnionDeep<[0, 1, 2] | [0, 1, 2] | [0, 1, 2] | [0, unknown] | ['0', unknown] | number[]>); // eslint-disable-line @typescript-eslint/no-duplicate-type-constituents
